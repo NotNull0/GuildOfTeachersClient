b6a0ced81eedb3431eb7c73d6f6c0dc3241a453d
Пользователь <Nazar.sekh@gmail.com>
2018-11-23 22:34:12 +0300

    fix

--- src/app/shared/config/url.ts
+++ src/app/shared/config/url.ts
@@ -1,7 +1,6 @@
 // export const url="http://kek.org:88888";
+export const url = 'http://localhost:9999';
 // export const url="http://192.168.1.34:9999";
-// export const url="http://192.168.1.34:9999";
-export const url="http://mplus.hopto.org:8888/010-server";
-// export const url="http://192.168.1.232:9090";
-//for server host very-very-very-very important
+// export const url="http://mplus.hopto.org:8888/010-server";
+// export const url = 'http://192.168.1.232:9090';
 // export const url="http://teachers.org.ua:8080";
